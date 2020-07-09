739c15a046e6654742c3834762a9163da38374f5
jp-caldeira <jpcm86@gmail.com>
2020-07-09 15:49:35 -0300

    exercicios 0907

--- exercicios/login.php
+++ exercicios/login.php
@@ -1,8 +1,8 @@
 <?php
 
-echo "var dump no post:";
-var_dump($_POST);
-echo "<br><br>";
+// echo "var dump no post:";
+// var_dump($_POST);
+// echo "<br><br>";
 
 $newEmail = $_POST['email'];
 
@@ -10,34 +10,27 @@ $newName = $_POST['nome'];
 
 $senhaC = password_hash($_POST['senha'], PASSWORD_DEFAULT);
 
-$arraydecode = file_get_contents('rgst.json');
-echo "var dump depois do get contents:";
-var_dump($arraydecode);
-echo "<br><br>";
+ $arraydecode = file_get_contents('rgst.json');
+ echo "var dump depois do get contents:";
+ var_dump($arraydecode);
+ echo "<br><br>";
+
+ $arrayUsers = json_decode($arraydecode, true);
+ echo "var dump depois do json_decode:";
+ var_dump($arrayUsers);
+ echo  "<br>";
 
 
-$array1 = json_decode($arraydecode, true);
-echo "var dump depois do json_decode:";
-var_dump($array1);
-echo  "<br>";
-
-
-if (in_array($newEmail, $array1)){
+if (in_array($newEmail, $arrayUsers[$newEmail])){
     echo "<br>E-mail já existe<br>";
 } else {
-   $userData = array("nome"=>$newName,"email"=> $newEmail, "senha"=> $senhaC);
-   $userData = json_encode($userData);
+   $arrayUsers[$newEmail] = array("nome"=>$newName,"email"=> $newEmail, "senha"=> $senhaC);
+   $userData = json_encode($arrayUsers);
    file_put_contents('rgst.json', $userData);
    echo "<br>Informações inseridas com sucesso!<br>";
    echo "var dump nas informações inseridas:<br>";
-   var_dump($userData);
-}
+  }
 
-// $arraydecode1 = file_get_contents('rgst.json');
-
-// $arraydecode1 = json_decode($arraydecode1, true);
-
-// var_dump($arraydecode1);
 
 
 
