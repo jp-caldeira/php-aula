107505680437b5c3a4f218fe247aba0346ef5d5c
jp-caldeira <jpcm86@gmail.com>
2020-07-16 12:40:38 -0300

    160720

--- exercicios/login.php
+++ exercicios/login.php
@@ -1,14 +1,29 @@
 <?php
 
-// echo "var dump no post:";
-// var_dump($_POST);
-// echo "<br><br>";
+//a. salvar as informações do formulário de inscrição em JSON com a senha criptografada usando password_hash
+//i. fazer a verificação da existência dos e-mails e, caso já exista uma conta com o email anterior, as informações
+//novas não devem ser salvas e o usuário deve ser avisado;
+//ii. atribuir ao usuário um id único, com númeração automática, usando como referência o último id inserido
+//no arquivo
 
-$newEmail = $_POST['email'];
+if(!isset($_POST['nome']) || $_POST['nome'] === ""){
+  echo "<br>Insira seu nome para continuar<br>";  
+  } else {
+    $newName = $_POST['nome'];
+  }
 
-$newName = $_POST['nome'];
+if (!isset($_POST['email'])){
+  echo "<br>Insira seu email para continuar<br>";
+} else {
+  $newEmail = $_POST['email'];
+}
 
+if (!isset($_POST['senha'])){
+  echo "<br>Digite uma senha para se registrar<br>";
+} else {
 $senhaC = password_hash($_POST['senha'], PASSWORD_DEFAULT);
+}
+
 
  $arraydecode = file_get_contents('rgst.json');
  // echo "var dump depois do get contents:";
@@ -34,9 +49,10 @@ if (isset($arrayUsers)){
     $arrayEmails[] = $row["email"];}
   }
 
-
 //coloca outro isset, depois um if dentro do if que vai avaliar se o newEmail está dentro do array Emails e aí sim
 //vai adicionar o valor;
+
+  if(isset($newEmail)){
     if (in_array($newEmail, $arrayEmails)){
       echo "<br>E-mail já existe<br>";
     } else {
@@ -45,7 +61,29 @@ if (isset($arrayUsers)){
    file_put_contents('rgst.json', $userData);
    echo "<br>Informações inseridas com sucesso!<br>";
   }
-
-
-
+}
+//b.em login, verificar a existência dos dados inseridos
+//i.verificar a existência do e-mail, se a senha está correta 
  ?>
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login</title>
+ </head>
+ <body>
+ <h3>Faça seu login:</h3>
+  <form action="login2.php" method='post'>
+    <label for="email">Email:</label>
+    <input type="email" name="email"></input>
+    <br>
+    <label for='senha'>Senha:</label>
+    <input type="password" name="senha"></input>
+    <br>
+    <input type="submit" name="Fazer login">
+  </form>
+   
+ </body>
+ </html>
---
+++ exercicios/login2.php
@@ -0,0 +1,12 @@
+<?php 
+
+//Em login, verificar a existência dos dados inseridos
+//i.verificar a existência do e-mail, se a senha está correta 
+
+var_dump($_POST);
+
+echo $_POST[0];
+echo $_POST[1];
+    
+
+?>
