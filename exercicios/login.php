e5d61c29b7006a1da02d6df09a0a7f3fc77bc99d
jp-caldeira <jpcm86@gmail.com>
2020-07-21 19:03:09 -0300

    exercicios e css

--- exercicios/login.php
+++ exercicios/login.php
@@ -1,39 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css">
+  </head>
+<body>
+
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">Novidades</a></li>
+        <li><a href="#contact">Contato</a></li>
+        <li><a href="#about">Sobre</a></li>
+      </ul>
+
+
+    <section class='container first-section'>
 <?php
-
-//a. salvar as informações do formulário de inscrição em JSON com a senha criptografada usando password_hash
-//i. fazer a verificação da existência dos e-mails e, caso já exista uma conta com o email anterior, as informações
-//novas não devem ser salvas e o usuário deve ser avisado;
-//ii. atribuir ao usuário um id único, com númeração automática, usando como referência o último id inserido
-//no arquivo
-
   if($_POST){
     $newName = trim($_POST['nome']);
     $newEmail = trim($_POST['email']);
     $senha = trim($_POST['senha']);
             if (strlen($newName) < 2){
-            echo "<br>ERRO: Você precisa digitar seu <strong>nome</strong> para se registrar<br>";
+            echo "<br>ERRO: Você precisa digitar seu <strong>nome</strong> para se registrar<br><a href='register.php'>Voltar</a>";
             }
             if (filter_var($newEmail, FILTER_VALIDATE_EMAIL) == false) {
-            echo "<br>ERRO:Você deve inserir um <strong>email válido</strong> para continuar<br>";
+            echo "<br>ERRO:Você deve inserir um <strong>email válido</strong> para continuar<br><a href='register.php'>Voltar</a>";
           }
           if (strlen($senha) < 6){
-            echo "<br>ERRO: O campo <strong>SENHA</strong> deve ter ao menos SEIS caracteres.<br>";
+            echo "<br>ERRO: O campo <strong>SENHA</strong> deve ter ao menos SEIS caracteres.<br><a href='register.php'>Voltar</a>";
           } else {
             $senhaC = password_hash($_POST['senha'], PASSWORD_DEFAULT);
           }
-    } else {
-      echo "<br>Insira suas informações<br>";
-    }
+    } else { ?>
+      <h1>Faça seu login:</h1>
+      <h2>(ou cadastre-se <a href='register.php'>aqui</a>)</h2>
+    <?php } ?>
+</section>
 
+<?php
 
  $arraydecode = file_get_contents('rgst.json');
 
  $arrayUsers = json_decode($arraydecode, true);
- 
+
 if (!isset($arrayUsers)){
     $id = 1;
   } else {
-    $lastId = array_key_last($arrayUsers);
+    $lastId = end($arrayUsers);
+    $lastId = $lastId['id'];
     $id = $lastId + 1;
 }
 
@@ -44,14 +63,12 @@ if (isset($arrayUsers)){
     $arrayEmails[] = $row["email"];}
   }
 
-//coloca outro isset, depois um if dentro do if que vai avaliar se o newEmail está dentro do array Emails e aí sim
-//vai adicionar o valor;
 
-  if(isset($newEmail) && isset($senhaC)){
+if(isset($newEmail) && isset($senhaC)){
     if (in_array($newEmail, $arrayEmails)){
       echo "<br>E-mail já existe<br>";
     } else {
-   $arrayUsers[$id] = array("nome"=>$newName,"email"=> $newEmail, "senha"=> $senhaC);
+   $arrayUsers[$newEmail] = array("id"=> $id, "nome"=>$newName,"email"=> $newEmail, "senha"=> $senhaC);
    $userData = json_encode($arrayUsers);
    file_put_contents('rgst.json', $userData);
    echo "<br>Informações inseridas com sucesso!<br>";
@@ -60,25 +77,45 @@ if (isset($arrayUsers)){
 //b.em login, verificar a existência dos dados inseridos
 //i.verificar a existência do e-mail, se a senha está correta
  ?>
+ <div class="container-fluid">
+     <section class="login-form">
+             <form action="login2.php" method='post'>
+              <label for="email">Email:</label>
+              <input type="email" name="email"></input>
+              <br>
+              <label for='senha'>Senha:</label>
+              <input type="password" name="senha"></input>
+              <br>
+              <input type="submit" name="Fazer login">
+            </form>
+      </section>
+    </div>
+  <div class="box">
+    bla bla bla<br>
+    bla bla bla<br>
+    bla bla bla<br>
+    bla bla bla<br>
+    bla bla bla<br>
+  </div>
+  <div class="box">
+    bla bla bla 2<br>
+    bla bla bla 2<br>
+    bla bla bla 2<br>
+    bla bla bla 2<br>
+    bla bla bla 2<br>
+  </div>
+  <div class="box">
+    bla bla bla 3<br>
+    bla bla bla 3<br>
+    bla bla bla 3<br>
+    bla bla bla 3<br>
+    bla bla bla 3<br>
+  </div>
 
- <!DOCTYPE html>
- <html lang="en">
- <head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Login</title>
- </head>
- <body>
- <h3>Faça seu login:</h3>
-  <form action="login2.php" method='post'>
-    <label for="email">Email:</label>
-    <input type="email" name="email"></input>
-    <br>
-    <label for='senha'>Senha:</label>
-    <input type="password" name="senha"></input>
-    <br>
-    <input type="submit" name="Fazer login">
-  </form>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
  </body>
- </html>
+</html>
--- exercicios/login2.php
+++ exercicios/login2.php
@@ -1,12 +1,47 @@
-<?php 
+<?php
 
 //Em login, verificar a existência dos dados inseridos
-//i.verificar a existência do e-mail, se a senha está correta 
+//i.verificar a existência do e-mail, se a senha está correta
 
-var_dump($_POST);
+if($_POST){
+$email = $_POST['email'];
+$passwordString = $_POST['senha'];
+}
 
-echo $_POST[0];
-echo $_POST[1];
-    
+$arraydecode = file_get_contents('rgst.json');
+
+$arrayUsers = json_decode($arraydecode, true);
+
+$arrayEmails = array();
+
+if (isset($arrayUsers)){
+  foreach ($arrayUsers as $row){
+    $arrayEmails[] = $row["email"];}
+  }
+
+
+if($_POST){
+if(in_array($email, $arrayEmails)){
+  echo "Email registrado<br>";
+} else { ?>
+  <p>Email não encontrado!</p>
+  <p>Clique em <strong>voltar</strong> para fazer seu cadastro:</p>
+  <br>
+  <button type="button" name="button"><a href='register.php'>Voltar</a></button>
+<br>
+<?php }
+
+if (password_verify($passwordString, $arrayUsers[$email]['senha'])){
+    echo "<br>Senha Correta";
+} else { ?>
+  <p>Senha INCORRETA!</p>
+  <p>Clique em <strong>voltar</strong>:</p>
+  <br>
+  <button type="button" name="button"><a href='login.php'>Voltar</a></button>
+  <br>
+<?php }
+} else {
+  echo "Insira suas informações<br><button type='button' name='button'><a href='login.php'>Voltar</a></button><br>";
+  }
 
 ?>
